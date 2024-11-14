455e8df691e7e52aa6e4db173ca276daf32cf730
aaron <aaronzzxup@gmail.com>
2024-11-14 18:32:20 +0800

    初步增加无障碍服务

--- app/src/main/java/com/aaron/sidegesture/MainActivity.kt
+++ app/src/main/java/com/aaron/sidegesture/MainActivity.kt
@@ -4,44 +4,185 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import com.aaron.sidegesture.ui.theme.SideGestureTheme
+import kotlin.math.asin
+import kotlin.math.hypot
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SideGestureTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SideSlideContainer()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun SideSlideContainer() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF7F7F7))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = AbsoluteAlignment.Left
+        ) {
+            val density = LocalDensity.current
+            val maxWidth = 40.dp
+            var firstPoint: Offset by remember { mutableStateOf(Offset.Zero) }
+            var secondPoint: Offset by remember { mutableStateOf(Offset.Zero) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SideGestureTheme {
-        Greeting("Android")
+            Box(
+                modifier = Modifier
+                    .width(60.dp)
+                    .height(600.dp)
+                    .pointerInput(Unit) {
+                        detectDragGestures(
+                            onDragStart = { offset ->
+                                firstPoint = offset.copy(x = 0f)
+                                secondPoint = offset.copy(x = 0f)
+                            },
+                            onDrag = { change, dragAmount ->
+                                density.run {
+                                    val temp = secondPoint + dragAmount
+                                    if (temp.x >= maxWidth.toPx() / 2f) {
+                                        secondPoint += dragAmount / 3f
+                                    } else {
+                                        secondPoint = temp
+                                    }
+                                }
+                            },
+                            onDragEnd = {
+                                firstPoint = Offset.Zero
+                                secondPoint = Offset.Zero
+                            },
+                            onDragCancel = {
+                                firstPoint = Offset.Zero
+                                secondPoint = Offset.Zero
+                            }
+                        )
+                    }
+            ) {
+                Box(
+                    modifier = Modifier
+                        .drawBehind {
+                            if (firstPoint != Offset.Zero && secondPoint != Offset.Zero) {
+                                val maxWidth = maxWidth.toPx()
+                                val bezierRadius = 80.dp.toPx()
+                                val firstPoint = firstPoint.copy(
+                                    y = firstPoint.y - 48.dp.toPx()
+                                )
+                                val secondPoint = secondPoint.copy(
+                                    x = secondPoint.x.coerceAtMost(maxWidth),
+                                    y = secondPoint.y - 48.dp.toPx()
+                                )
+                                val path = Path().also {
+                                    it.moveTo(0f, firstPoint.y - bezierRadius)
+                                    it.cubicTo(
+                                        x1 = 4f,
+                                        y1 = firstPoint.y - bezierRadius / 2f,
+                                        x2 = secondPoint.x,
+                                        y2 = firstPoint.y - bezierRadius / 2f,
+                                        x3 = secondPoint.x,
+                                        y3 = firstPoint.y
+                                    )
+                                    it.cubicTo(
+                                        x1 = secondPoint.x,
+                                        y1 = firstPoint.y + bezierRadius / 2f,
+                                        x2 = 4f,
+                                        y2 = firstPoint.y + bezierRadius / 2f,
+                                        x3 = 0f,
+                                        y3 = firstPoint.y + bezierRadius
+                                    )
+                                }
+                                drawPath(
+                                    path = path,
+                                    color = Color.Black
+                                )
+                                drawPath(
+                                    path = path,
+                                    color = Color(0xFFBDBDBD),
+                                    style = Stroke(width = 1.dp.toPx())
+                                )
+                            }
+                        },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        modifier = Modifier
+                            .size(24.dp)
+                            .graphicsLayer {
+                                val firstY = firstPoint.y
+                                val secondX = secondPoint.x
+                                val secondY = secondPoint.y
+                                val offsetY = secondY - firstY
+                                val hypotenuse = hypot(offsetY, secondX)
+                                val radians = asin(secondX / hypotenuse)
+                                val degrees = Math.toDegrees(radians.toDouble())
+                                if (degrees <= 60f) {
+                                    if (offsetY < 0) {
+                                        // up
+                                        rotationZ = -45f
+                                    } else {
+                                        // down
+                                        rotationZ = 45f
+                                    }
+                                } else {
+                                    rotationZ = 0f
+                                }
+
+                                translationX = 6.dp.toPx()
+                                translationY = (-48).dp.toPx() + firstPoint.y - 12.dp.toPx()
+
+                                scaleX = secondPoint.x.coerceAtMost(maxWidth.toPx()) / maxWidth.toPx()
+                                scaleY = scaleX
+                            },
+                        imageVector = Icons.Default.ArrowForward,
+                        contentDescription = "arrow",
+                        colorFilter = ColorFilter.tint(color = Color.White.copy(alpha = 0.7f))
+                    )
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/aaron/sidegesture/SideGestureService.kt
+++ app/src/main/java/com/aaron/sidegesture/SideGestureService.kt
@@ -1,7 +1,20 @@
 package com.aaron.sidegesture
 
+import android.content.Intent
+import android.util.Log
 import android.view.accessibility.AccessibilityEvent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.aaron.compose.ktx.clipToBackground
+import com.aaron.compose.ktx.onSingleClick
 import com.aaron.composeaccessibility.ComponentAccessibilityService
+import com.aaron.composeaccessibility.setComposeOverlay
 
 /**
  * @author aaronzzxup@gmail.com
@@ -9,12 +22,57 @@ import com.aaron.composeaccessibility.ComponentAccessibilityService
  */
 class SideGestureService : ComponentAccessibilityService() {
 
+    private var prevPkgName: String? = null
+    private var curPkgName: String? = null
+
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
+        when(p0?.eventType){
+            AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED -> {
+                val pkgName = p0.packageName?.toString() ?: ""
+                val intent = packageManager.getLaunchIntentForPackage(pkgName)
+                if (intent != null && curPkgName != pkgName) {
+                    prevPkgName = curPkgName
+                    curPkgName = pkgName
+                }
+            }
+            else -> Unit
+        }
     }
 
     override fun onInterrupt() {
     }
 
     override fun onSetOverlay() {
+        setComposeOverlay(this, this, this) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .size(48.dp)
+                        .clipToBackground(
+                            color = Color.Red,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .onSingleClick {
+                            test()
+                        }
+                )
+            }
+        }
+    }
+
+    private fun test() {
+        val prevPkgName = prevPkgName ?: return
+        val curPkgName = curPkgName
+        if (prevPkgName == curPkgName) return
+        val intent = packageManager.getLaunchIntentForPackage(prevPkgName) ?: return
+        try {
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            this.prevPkgName = curPkgName
+            this.curPkgName = prevPkgName
+        } catch (ex: Exception) {
+            Log.d("zzx", "$ex")
+        }
     }
 }
