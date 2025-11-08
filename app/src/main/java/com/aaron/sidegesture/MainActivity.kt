073ae5852a971bbd21e4cc11048a4cf9d63042f6
aaron <aaronzzxup@gmail.com>
2025-11-08 13:43:49 +0800

    处理导航栏颜色错误

--- app/src/main/java/com/aaron/sidegesture/MainActivity.kt
+++ app/src/main/java/com/aaron/sidegesture/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aaron.sidegesture
 import android.app.ActivityManager
 import android.content.Context
 import android.content.res.Configuration
+import android.content.res.Resources
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -30,24 +31,28 @@ class MainActivity : ComponentActivity() {
             val am = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
             DataStoreHolder.advancedSettings.data.collectLatest { item ->
                 am.appTasks.firstOrNull()?.setExcludeFromRecents(item.excludeFromRecents)
-                myEnableEdgeToEdge(item.dayNightMode == DayNightMode.Night)
+                myEnableEdgeToEdge(item.dayNightMode)
             }
         }
     }
 }
 
-private fun ComponentActivity.myEnableEdgeToEdge(isAppNightMode: Boolean = false) {
-    enableEdgeToEdge(
-        statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT) { resources ->
-            val flag = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-            flag == Configuration.UI_MODE_NIGHT_YES || isAppNightMode
-        },
-        navigationBarStyle = SystemBarStyle.auto(DefaultLightScrim, DefaultDarkScrim) { resources ->
-            val flag = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
-            flag == Configuration.UI_MODE_NIGHT_YES || isAppNightMode
+private fun ComponentActivity.myEnableEdgeToEdge(dayNightMode: DayNightMode = DayNightMode.Auto) {
+    val block: (Resources) -> Boolean = block@{ resources ->
+        if (dayNightMode != DayNightMode.Auto) {
+            return@block when (dayNightMode) {
+                DayNightMode.Night -> true
+                else -> false
+            }
         }
+        val flags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+        flags == Configuration.UI_MODE_NIGHT_YES
+    }
+    enableEdgeToEdge(
+        statusBarStyle = SystemBarStyle.auto(Color.TRANSPARENT, Color.TRANSPARENT, block),
+        navigationBarStyle = SystemBarStyle.auto(DefaultLightScrim, DefaultDarkScrim, block)
     )
 }
 
-private val DefaultLightScrim = Color.argb(0xe6, 0xFF, 0xFF, 0xFF)
-private val DefaultDarkScrim = Color.argb(0x80, 0x1b, 0x1b, 0x1b)
+private val DefaultLightScrim = Color.argb(0xBF, 0xFF, 0xFF, 0xFF)
+private val DefaultDarkScrim = Color.argb(0xBF, 0x00, 0x00, 0x00)
