305bf5c0f17d0d2a7e39af6ab641ff75ad0eddd6
aaron <aaronzzxup@gmail.com>
2025-11-09 21:02:25 +0800

    v1.4.3-release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "gulu.gulugulu"
         minSdk = 23
         targetSdk = 35
-        versionCode = 10402
-        versionName = "1.4.2"
+        versionCode = 10403
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
